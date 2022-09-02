8b9eb750108ae744d1395c389cb915337b34f4da
Trevon Mays <103228576+TrevonMays@users.noreply.github.com>
2022-09-01 17:24:05 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-const pokémon = Object.freeze([
-    { "id": 1, "name": "Bulbasaur", "types": ["poison", "grass"] },
-    { "id": 5, "name": "Charmeleon", "types": ["fire"] },
-    { "id": 9, "name": "Blastoise", "types": ["water"] },
-    { "id": 12, "name": "Butterfree", "types": ["bug", "flying"] },
-    { "id": 16, "name": "Pidgey", "types": ["normal", "flying"] },
-    { "id": 23, "name": "Ekans", "types": ["poison"] },
-    { "id": 24, "name": "Arbok", "types": ["poison"] },
-    { "id": 25, "name": "Pikachu", "types": ["electric"] },
-    { "id": 37, "name": "Vulpix", "types": ["fire"] },
-    { "id": 52, "name": "Meowth", "types": ["normal"] },
-    { "id": 63, "name": "Abra", "types": ["psychic"] },
-    { "id": 67, "name": "Machamp", "types": ["fighting"] },
-    { "id": 72, "name": "Tentacool", "types": ["water", "poison"] },
-    { "id": 74, "name": "Geodude", "types": ["rock", "ground"] },
-    { "id": 87, "name": "Dewgong", "types": ["water", "ice"] },
-    { "id": 98, "name": "Krabby", "types": ["water"] },
-    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
-    { "id": 122, "name": "Mr. Mime", "types": ["psychic"] },
-    { "id": 133, "name": "Eevee", "types": ["normal"] },
-    { "id": 144, "name": "Articuno", "types": ["ice", "flying"] },
-    { "id": 145, "name": "Zapdos", "types": ["electric", "flying"] },
-    { "id": 146, "name": "Moltres", "types": ["fire", "flying"] },
-    { "id": 148, "name": "Dragonair", "types": ["dragon"] }
-]);
+ class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 0;
+        this.speed = 3;
+        this.strength = 3;
+        this.drink = this.drink
+    }
+    sayName(){
+        console.log(`${this.name}`)
+        return this
+    }
+    drink(){
 
-const bListPkmn = pokémon.filter( p => p.name[0] === "B" );
-const pkmnIds = pokémon.map( p => p.id )
-const  divByThree = pokémon.filter(p => p.id % 3 === 0)
-const firePoke = pokémon.filter(p => p.types[0] === "fire")
-const duoTypes = pokémon.filter(p => p.types.length>1)
-const justNames = pokémon.map(p => p.name)
-const powerLevelOver = pokémon.filter(p => p.id > 99)
-const poisonPoke = pokémon.filter(p => p.types[0] === "poison")
-const flyingPoke = pokémon.filter(p => p.types[1] === "flying").map(p => p.types[0])
-const normie = pokémon.filter(p => p.types[0] === "normal")
+    }
+    showStats(){
+        console.log(`
+        Name: ${this.name}
+        Health: ${this.health}
+        Speed: ${this.speed}
+        Strength: ${this.strength} 
+        `)
+        return this
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
 
 
-console.log(flyingPoke)
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name)
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10
+    }
+speakWisdom(){
+    console.log("What one programer can do in one month, two programmers can do in two months.")
+    return this
+}
+}
+const newSensei = new Sensei("trey")
+console.log(newSensei)
+newSensei.speakWisdom()
